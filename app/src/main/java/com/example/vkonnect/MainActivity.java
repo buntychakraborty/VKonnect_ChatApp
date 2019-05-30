c210a92bcea3318395473525fb82c460a6929082
bunty <buntychakraborty@outlook.com>
2019-05-30 07:29:28 +0530

    hello---1

--- app/src/main/java/com/example/vkonnect/MainActivity.java
+++ app/src/main/java/com/example/vkonnect/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.vkonnect;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBar;
@@ -10,9 +11,15 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -22,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
     private TabsAcessorAdapter myTabsAcessorAdapter;
     private FirebaseUser currentUser;
     private FirebaseAuth mAuth;
+    private DatabaseReference rootRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,21 +40,12 @@ public class MainActivity extends AppCompatActivity {
 
         mAuth = FirebaseAuth.getInstance();
         currentUser = mAuth.getCurrentUser();
-
-       // mToolbar = (Toolbar) findViewById(R.id.main_page_tool);
-       // mToolbar.inflateMenu(R.menu.menu_options);
-
-       // setSupportActionBar(mToolbar);
-
-        //getSupportActionBar().setTitle("VKonnect");
-
-
-
         myViewPager = (ViewPager) findViewById(R.id.main_tabs_pager);
         myTabsAcessorAdapter = new TabsAcessorAdapter(getSupportFragmentManager());
         myViewPager.setAdapter(myTabsAcessorAdapter);
         myTabLayout = (TabLayout) findViewById(R.id.main_tabs);
         myTabLayout.setupWithViewPager(myViewPager);
+        rootRef= FirebaseDatabase.getInstance().getReference();
 
 
     }
@@ -59,22 +58,53 @@ public class MainActivity extends AppCompatActivity {
 
             sendUserToLoginPage();
         }
+        else{
+            verifyUserExistence();
+        }
+    }
+
+    private void verifyUserExistence() {
+
+        String currentUserId=mAuth.getCurrentUser().getUid();
+        rootRef.child("Users").child(currentUserId).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(dataSnapshot.child("name").exists()){
+                    Toast.makeText(MainActivity.this,"Welcome",Toast.LENGTH_SHORT).show(); //user has done all settings
+                }
+
+                else{
+                    sendUserToSettingActivity();//If user has created his account and he needs to do system setup
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+
+    }
+
+    private void sendUserToSettingActivity() {
+        Intent settingIntent=new Intent(getApplicationContext(),SettingActivity.class);
+        settingIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK); //cannot press back button to go back
+        startActivity(settingIntent);
+        finish();
     }
 
     private void sendUserToLoginPage() {
         Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK); //cannot press back button to go back
         startActivity(intent);
+        finish();
 
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-
-        //super.onCreateOptionsMenu(menu);
-        getMenuInflater().inflate(R.menu.menu_options,menu);
-
-       // setSupportActionBar(mToolbar);
-
+        getMenuInflater().inflate(R.menu.menu_options, menu);
         return true;
     }
 
@@ -87,6 +117,7 @@ public class MainActivity extends AppCompatActivity {
             return true;
         }
         if (item.getItemId() == R.id.settings) {
+            sendUserToSettingActivity();
             return true;
 
         }
--- app/src/main/java/com/example/vkonnect/SettingActivity.java
+++ app/src/main/java/com/example/vkonnect/SettingActivity.java
@@ -1,13 +1,106 @@
 package com.example.vkonnect;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class SettingActivity extends AppCompatActivity {
+    private EditText setUserName,setStatus;
+    private Button updateSettings;
+    private CircleImageView userProfileImage;
+    private DatabaseReference rootRef;
+    private FirebaseAuth mAuth;
+    private String currentUid;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+        initializeItems();
+        updateSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateSetting();
+            }
+        });
+
+
+
     }
+
+
+
+    private void initializeItems() {
+        setUserName=(EditText)findViewById(R.id.set_user_name);
+        setStatus=(EditText)findViewById(R.id.set_profile_status);
+        userProfileImage=(CircleImageView)findViewById(R.id.set_profile_image);
+        updateSettings=(Button)findViewById(R.id.update_settings);
+        mAuth=FirebaseAuth.getInstance();
+
+        currentUid=mAuth.getUid();
+        rootRef= FirebaseDatabase.getInstance().getReference();
+
+
+    }
+    private void updateSetting() {
+        String setUserNameFor=setUserName.getText().toString();
+        String setUserStatus=setStatus.getText().toString();
+
+        if(TextUtils.isEmpty(setUserNameFor)){
+            Toast.makeText(this,"Please enter your username...",Toast.LENGTH_SHORT).show();
+        }
+
+        if(TextUtils.isEmpty(setUserStatus)){
+            Toast.makeText(this,"Please enter your satus....",Toast.LENGTH_SHORT).show();
+        }
+        else{
+            HashMap<String,String> profileMap=new HashMap<>();
+            profileMap.put("uid",currentUid);
+            profileMap.put("name",setUserNameFor);
+            profileMap.put("status",setUserStatus);
+            rootRef.child("Users").child(currentUid).setValue(profileMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(@NonNull Task<Void> task) {
+                    if(task.isSuccessful()){
+                        sendUserToMainActivity();
+                        Toast.makeText(getApplicationContext(),"Profile Updated Sucessfully....",Toast.LENGTH_SHORT).show();
+
+                    }
+                    else{
+                        String errorMessage = task.getException().toString();
+                        Toast.makeText(getApplicationContext(), "Error : " + errorMessage, Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+
+
+        }
+
+
+    }
+
+    private void sendUserToMainActivity() {
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK); //cannot press back button to go back
+        startActivity(intent);
+        finish();
+    }
+
 }
